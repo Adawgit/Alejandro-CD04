264a4ec28b834c537c229f63d1eec3676e9c2814
Alex <Alex@10.0.2.15>
2022-01-27 06:03:08 +0100

    Primer Commit CD04

--- eclipse-workspace/Deposito/src/cuentas/Main.java
+++ eclipse-workspace/Deposito/src/cuentas/Main.java
@@ -6,18 +6,18 @@ public class Main {
    operativa_cuenta ();
     }
    public static void operativa_cuenta() {
-}
+
    		float cantidad;
    		CCuenta cuenta1;
-        double saldoActual;
-
+   		double saldoActual;
+   	
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
             cuenta1.retirar(2300);
-        } catch (Exception e) {
+        } catch (Exception e1) {
             System.out.print("Fallo al retirar");
         }
         try {
